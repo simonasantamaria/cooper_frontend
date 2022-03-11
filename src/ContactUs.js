4a762ed9758353433cc1c4f5b19bea9b72e602de
hassan1731996 <hassan1731996@gmail.com>
2022-03-11 15:35:54 +0500

    added axios as dev-dependency in package.json file

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from 'react';
 import emailjs from 'emailjs-com';
 
 import Box from '@mui/material/Box';
@@ -33,17 +33,16 @@ import Input from '@mui/material/Input';
 import Stack from '@mui/material/Stack';
 
 
-
-
 import "./style.css";
 import pirates_logo from './pirates_logo.png';
 import './App.css';
-import { Button, Typography } from '@material-ui/core';
+import {Button, Typography} from '@material-ui/core';
 import TodoList from './TodoList';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import axios from "axios";
+import {BACKEND_URL} from "./constants";
 
 const SERVICE_ID = "service_cgboddf";
 const TEMPLATE_ID = "template_7vfo4ds";
@@ -52,154 +51,153 @@ const USER_ID = "user_f3CQB1im3popVTlDE3kA3";
 
 const ContactUs = () => {
 
-  const [name, setName] = useState('Simona');
-  const [email, setEmail] = useState('simona.santamaria1@gmail.com');
+    const [name, setName] = useState('Simona');
+    const [email, setEmail] = useState('simona.santamaria1@gmail.com');
 
-  const useStyles = makeStyles({
-    changeStyle: {
-      fontStyle: "normal",
-      color: "grey",
-      fontsize: "30px"
-    },
-    root: {
-      display: "flex",
-    },
-    drawer: {
-      width: 240,
-      zIndex: 400,
-      flexShrink: 0
-      /*whiteSpace: "nowrap",*/
-    },
-    boxBackground: {
-      backgroundColor: "white",
-      fontSize: 30,
-    },
-    boxBackground2: {
-      backgroundColor: "white",
-      fontSize: 30
-    },
-    boxBackground3: {
-      backgroundColor: "white",
-      fontSize: 30
-    },
+    const useStyles = makeStyles({
+        changeStyle: {
+            fontStyle: "normal",
+            color: "grey",
+            fontsize: "30px"
+        },
+        root: {
+            display: "flex",
+        },
+        drawer: {
+            width: 240,
+            zIndex: 400,
+            flexShrink: 0
+            /*whiteSpace: "nowrap",*/
+        },
+        boxBackground: {
+            backgroundColor: "white",
+            fontSize: 30,
+        },
+        boxBackground2: {
+            backgroundColor: "white",
+            fontSize: 30
+        },
+        boxBackground3: {
+            backgroundColor: "white",
+            fontSize: 30
+        },
 
-    gridhidden: {
-      background: 'white',
-      color: 'white',
-      height: 40,
-      padding: '0 30px',
-      elevation: 0,
-      borderBottom: "none"
-    },
-  })
-
-  const classes = useStyles();
-
-
-  const [dataset, setCurrency] = React.useState('Chest-X-ray-images');
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrency(event.target.value);
-  };
-
-
-  const datasets = [
-    {
-      value: 'Chest-X-ray-images',
-      label: 'Chest-X-ray-images',
-    },
-  ];
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    // POST request to send payload to BAck-End
-    axios.post(`http://127.0.0.1:8000/api/v1/print-variables/`, {
-      "name": name,
-      "email": email
+        gridhidden: {
+            background: 'white',
+            color: 'white',
+            height: 40,
+            padding: '0 30px',
+            elevation: 0,
+            borderBottom: "none"
+        },
     })
-        .then(res => {
-          // console.log(res);
-          if (res.status === 200) {
-            console.log(res.data.response);
-          }
 
+    const classes = useStyles();
+
+
+    const [dataset, setCurrency] = React.useState('Chest-X-ray-images');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrency(event.target.value);
+    };
+
+
+    const datasets = [
+        {
+            value: 'Chest-X-ray-images',
+            label: 'Chest-X-ray-images',
+        },
+    ];
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        // POST request to send payload to BAck-End
+        axios.post({BACKEND_URL}`api/v1/print-variables/`, {
+            "name": name,
+            "email": email
         })
-    emailjs.send(SERVICE_ID, TEMPLATE_ID, {
-      to_email: email,
-      to_name: name,
-    }, USER_ID);
-  };
+            .then(res => {
+                // console.log(res);
+                if (res.status === 200) {
+                    console.log(res.data.response);
+                }
 
-  return (
+            })
+        emailjs.send(SERVICE_ID, TEMPLATE_ID, {
+            to_email: email,
+            to_name: name,
+        }, USER_ID);
+    };
 
-    <form>
-      <div>
+    return (
 
-        <Stack
-          component="form"
-          sx={{
-            width: '40ch',
-          }}
-          spacing={6}
-          noValidate
-          autoComplete="off"
-        >
+        <form>
+            <div>
 
-          <TextField
-            required
-            id="user_name"
-            label="Name"
-            defaultValue="Simona"
-            variant="standard"
-            name="user_name"
-            type="text"
-            onChange={e => setName(e.target.value)}
-          />
+                <Stack
+                    component="form"
+                    sx={{
+                        width: '40ch',
+                    }}
+                    spacing={6}
+                    noValidate
+                    autoComplete="off"
+                >
 
-          <TextField
-            required
-            id="user_email"
-            label="Email ID"
-            defaultValue="simona.santamaria1@gmail.com"
-            variant="standard"
-            name="user_email"
-            onChange={e => setEmail(e.target.value)}
-          />
+                    <TextField
+                        required
+                        id="user_name"
+                        label="Name"
+                        defaultValue="Simona"
+                        variant="standard"
+                        name="user_name"
+                        type="text"
+                        onChange={e => setName(e.target.value)}
+                    />
 
-          <TextField
-            id="outlined-select-currency"
-            select
-            label="Dataset"
-            value={dataset}
-            onChange={handleChange}
-          >
-            {datasets.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
+                    <TextField
+                        required
+                        id="user_email"
+                        label="Email ID"
+                        defaultValue="simona.santamaria1@gmail.com"
+                        variant="standard"
+                        name="user_email"
+                        onChange={e => setEmail(e.target.value)}
+                    />
 
-          <TextField
-            required
-            id="dep"
-            label="Departement"
-            defaultValue="Human Resources"
-            variant="standard"
-            name="dep"
-          />
+                    <TextField
+                        id="outlined-select-currency"
+                        select
+                        label="Dataset"
+                        value={dataset}
+                        onChange={handleChange}
+                    >
+                        {datasets.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
 
-
-          <Button variant="contained" onClick={(e)=> sendEmail(e)} color="primary" class="Center" >Data Anonymisation</Button>
-
-        </Stack>
-      </div>
-    </form>
-
-
+                    <TextField
+                        required
+                        id="dep"
+                        label="Departement"
+                        defaultValue="Human Resources"
+                        variant="standard"
+                        name="dep"
+                    />
 
 
-  );
+                    <Button variant="contained" onClick={(e) => sendEmail(e)} color="primary" class="Center">Data
+                        Anonymisation</Button>
+
+                </Stack>
+            </div>
+        </form>
+
+
+    );
 };
 
 export default ContactUs;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://cooper-b.herokuapp.com/"
